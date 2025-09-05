449c9a67ddee434e623981703c9df6481973fde2
lbianlbian <83378530+lbianlbian@users.noreply.github.com>
2025-09-04 17:07:18 -0700

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -1,7 +1,4 @@
-from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
 
-processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch16")
-processor.save_pretrained("./clip-vit")
+import clip
 
-model = AutoModelForZeroShotImageClassification.from_pretrained("openai/clip-vit-base-patch16")
-model.save_pretrained("./clip-vit")
+model, preprocess = clip.load("ViT-B/32", device="cpu")
